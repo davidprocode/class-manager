85ba62a77e8c51e66e2f41c7d00745a1a56991ec
davidprocode <davidprocode@proton.me>
2024-07-19 13:35:12 -0300

    create admin.users view

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,6 +9,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        return "UserController.index";
+        return view("admin.users");
     }
 }
---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Admin - Users</h1>
+    <span>
+        He who is contented is rich. - Laozi
+    </span>
+</div>
