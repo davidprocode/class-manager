9d4897d659206deff5d27e355fcbafc8181ba0bc
davidprocode <davidprocode@proton.me>
2024-07-19 13:54:06 -0300

    add Admin to nav

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view("admin.users");
+        $users = User::all();
+        return view('admin.index', compact('users'));
     }
 }
